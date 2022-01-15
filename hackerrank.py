7f6e77367c4bffcabc2b498f667d465aa7e20e49
Kist <kbear.swe@gmail.com>
2022-01-14 19:24:33 -0600

    update

--- hackerrank.py
+++ hackerrank.py
@@ -1,14 +1,14 @@
 # if else problem
 if __name__ == '__main__':
     n = int(input().strip())
-    if n % 2 !=0 or n % 2 == 0 and n in range(6,20):
+    if n % 2 != 0 or n % 2 == 0 and n in range(6, 20):
         print("Weird")
-    elif (n % 2 == 0 and n in range(2,5)):
+    elif (n % 2 == 0 and n in range(2, 5)):
         print("Not Weird")
     else:
         print("Not Weird")
 
-#division problem
+# division problem
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
-    print( a + b)
+    print(a + b)
     print(a - b)
     print(a * b)
 
@@ -31,16 +31,32 @@ def is_leap(year):
     if year % 4 == 0 and year % 400 == 0:
         return True
     elif year % 4 == 0 and year % 100 == 0:
-        return  False
+        return False
     else:
-        return False        
+        return False
     return leap
+
 
 year = int(input())
 
 
-#loops problem
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(0, n):
-        print(i**2)
+# loops problem
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 4 == 0 and year % 400 == 0:
+        return True
+    elif year % 4 == 0 and year % 100 == 0 or year % 4 != 0:
+        return False
+    elif year % 100 == 0 and year % 400 == 0:
+        return True
+    else:
+        return True
+    return leap
+
+year = int(input())
+print(is_leap(year))
+test 5 - 1992
+test 1 - 2100
